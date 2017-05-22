df1b4193af863583f707b76a312e325609144a2e
Bates <bates@Batess-iMac.local>
2017-05-22 15:09:31 -0400

    Removed old comments

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 /*polyline-midpoint
 * Takes an encoded polyline and returns midpoint as geoJSON
-*
-* @name polyine-midpoint
-* @param encoded polyline
-* @returns {geojson} point at midpoint
-* @example 
-* var encodedPolyline = 'yhg~FzrmvOkuAo~GkEg{Aer@mlB';
-* var midpoint = midpoint(encodedPolyline)
 */
 
 var polylineUtils = require('@mapbox/polyline');
@@ -14,16 +7,9 @@ var along = require('@turf/along');
 var lineDistance = require('@turf/line-distance');
 var helpers = require('@turf/helpers');
 
-//function to get polyline and midpoint geometry using Turf.js
+
 module.exports = function(encodedPolyline){
   var polylineArray = polylineUtils.decode(encodedPolyline);
-  //turf needs coords in lon/lat not lat/lng
-  // var turfArray = JSON.parse(JSON.stringify(polylineArray));;
-  // if(turfArray.length === polylineArray.length){
-  //   for (i=0;i<turfArray.length;i++){
-  //     turfArray[i].reverse();
-  //   }
-  // }
 
   for(i=0;i<polylineArray.length;i++){
     polylineArray[i].reverse();
